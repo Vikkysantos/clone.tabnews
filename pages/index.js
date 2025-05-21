7c5a8ef5e06da8b36b8942d1a3a817ec8417b636
Vitoria Santoss <vicksantos348@gmail.com>
2025-05-21 15:40:10 +0000

    adiciona a alteração no pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 function Home() {
   return (
     <>
-      <h1> IGOR teste este ahhh</h1>
+      <h1> IGOR e Vitoria</h1>
       <img src="/teste.jpg" />
       <img src="/dog.png" />
     </>
   );
 }
+
 export default Home;
